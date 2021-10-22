67d479bfe6c90be51bd75d8019fd2b3bb78da1be
oscar369 <oscar77rama@gmail.com>
2021-10-22 11:33:22 +0200

    Commit inicial

---
+++ propuesto2.java
@@ -0,0 +1,83 @@
+package EjerciciosPropuestos;
+ 
+public class propuesto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		x= 1;
+		int y;
+		y= 2;
+		double m;
+		m= 3.2;
+		double n;
+		n= 4.7;
+		int suma;
+		suma= x+y;
+		int resta;
+		resta=x-y;
+		int multiplicacion;
+		multiplicacion= x*y;
+		int division;
+		division= x/y;
+		int resto;
+		resto= x%y;
+		double sum;
+		sum= n+m;
+		double rest;
+		rest = n-m;
+		double producto;
+		producto= n*m;
+		double cociente;
+		cociente= n/m;
+		double rst;
+		rst= n%m;
+		double sumatres;
+		sumatres= x+n;
+		double div2;
+		div2= y/m;
+		double rest2;
+		rest2= y%m;
+		int dob;
+		dob = x*2;
+		int por2;
+		por2= y*2;
+		double doblando;
+		doblando= m*2;
+		double doble;
+		doble= n*2;
+		double sumavar;
+		sumavar= x+y+m+n;
+		double productovar;
+		productovar= x*y*m*n;
+		
+		
+		
+		
+		System.out.println("x es igual a : " + x);
+		System.out.println("y es igual a : " + y);
+		System.out.println("m es igual a : " + m);
+		System.out.println("n es igual a : " + n);
+		System.out.println("la suma de x+y es: " + suma);
+		System.out.println("la resta de x-y es: " + resta);
+		System.out.println("la multiplicación (x*y) da como resultado: " + multiplicacion);
+		System.out.println("la division (x/y) da como resultado: " + division);
+		System.out.println("El resto de la división (x/y) da como resultado: " + resto);
+		System.out.println("La suma de n+m es: " + sum);
+		System.out.println("La resta de n-m es: " + rest);
+		System.out.println("La multiplicacion (n*m) da como resultado : " + producto);
+		System.out.println("La división (n/m) da como resultado : " + cociente);
+		System.out.println("El resto de la  división (n/m) da como resultado : " + rst);
+		System.out.println("La suma de x+n es: " + sumatres);
+		System.out.println("la division (y/m) da como resultado: " + div2);
+		System.out.println("El resto de la  división (y/m) da como resultado : " + rest2);
+		System.out.println("El doble de x es: " + dob);
+		System.out.println("El doble de y es: " + por2);
+		System.out.println("El doble de n es: " + doblando);
+		System.out.println("El doble de m es: " + doble);
+		System.out.println("La suma de todas las variables es: " + sumavar);
+		System.out.println("La multiplicación de todas las variables es: " + productovar);
+		
+	}
+
+}
